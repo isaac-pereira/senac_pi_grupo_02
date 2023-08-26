1ff27c099938e491098c791a3ede1afc9b736fc9
Isaac Pereira <iespereira@gmail.com>
2023-08-26 19:54:14 -0300

    Incluídas funcionalidades de login e logout

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ bcrypt = Bcrypt(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -50,15 +50,16 @@ class RegisterForm(FlaskForm):
         existing_user_email = User.query.filter_by(email=email.data).first()
         if existing_user_email:
             raise ValidationError('E-mail de usuário já cadastrado.')
+    
+    def validate_cpf(self, cpf):
+        existing_user_cpf = User.query.filter_by(cpf=cpf.data).first()
+        if existing_user_cpf:
+            raise ValidationError('CPF de usuário já cadastrado.')
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField('Login')
+    email = StringField('E-mail', validators=[InputRequired(), Length(min=4, max=100)], render_kw={"placeholder": "Digite seu e-mail..."})
+    senha = PasswordField('Senha', validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Digite sua senha..."})
+    submit = SubmitField('Entrar')
 
 # Roteamento do flask (direciona para as devidas páginas html)
 @app.route('/', methods=['POST','GET'])
@@ -67,7 +68,22 @@ def index():
 
 @app.route('/login', methods=['POST','GET'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.senha, form.senha.data):
+                login_user(user)
+                return redirect(url_for('pesquisa'))
+            
+    return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['POST', 'GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/cadastro', methods=['POST','GET'])
 def cadastro():
@@ -78,13 +94,19 @@ def cadastro():
         novo_usuario = User(nome=form.nome.data, email=form.email.data, cpf=form.cpf.data, senha=hash_senha, cep=form.cep.data)
         db.session.add(novo_usuario)
         db.session.commit()
-        return redirect(url_for('login', id=novo_usuario.id))
+        return redirect(url_for('login'))
     
     return render_template('cadastro.html', form=form)
 
 @app.route('/pesquisa', methods=['POST','GET'])
+@login_required
 def pesquisa():
-    return render_template('pesquisa.html')
+    userid = current_user.get_id()
+    user = User.query.filter_by(id=userid).first()
+    username = user.nome
+    nomecompleto = username.split()
+    nome = nomecompleto[0]
+    return render_template('pesquisa.html', nome=nome)
 
 if __name__ == "__main__":
     app.run(debug=True)
